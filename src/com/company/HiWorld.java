6765192f1de07a074b8398dedaa8d3601243a402
renshenghaonan <1103172598@qq.com>
2020-04-21 16:32:08 +0800

    test

--- src/com/company/HiWorld.java
+++ src/com/company/HiWorld.java
@@ -3,6 +3,6 @@ package com.company;
 public class HiWorld{
 
     public static void main(String[] args) {
-        System.out.println("Hi World啊啊啊啊啊");
+        System.out.println("Hi World啊啊啊啊啊啊");
     }
 }
